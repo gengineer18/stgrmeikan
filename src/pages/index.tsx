364c33e4b2bd254349a85729e39f4513c292af6d
妹尾弦 <gengineer.web@gmail.com>
2020-07-03 22:24:31 +0900

    add: Material-ui

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,13 @@
 import App from '../components/App'
+import Button from '@material-ui/core/Button';
 
 export default function Home() {
   return (
     <App>
       <p>Index Page by TypeScript!</p>
+      <Button variant="contained" color="primary">
+        Hello World
+      </Button>
     </App>
   )
 }

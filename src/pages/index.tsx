6ead5215a3c3135a5eefe4258abcd8ae5f27c372
gengineer18 <51533184+gengineer18@users.noreply.github.com>
2020-07-03 23:27:30 +0900

    add: emotion (#3)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,23 @@
 import App from '../components/App'
 import Button from '@material-ui/core/Button';
+import styled from '@emotion/styled';
+
+const StButton = styled(Button)`
+  background: linear-gradient(45deg, #fe6b8b 30%, #ff8e53 90%);
+  border-radius: 3px;
+  border: 0;
+  color: white;
+  height: 48px;
+  padding: 0 30px;
+  box-shadow: 0 3px 5px 2px rgba(255, 105, 135, .3);
+`;
 
 export default function Home() {
   return (
     <App>
-      <p>Index Page by TypeScript!</p>
-      <Button variant="contained" color="primary">
-        Hello World
-      </Button>
+      <StButton variant="contained" color="primary">
+        Hello World!!
+      </StButton>
     </App>
   )
 }

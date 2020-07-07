8b2e1806ee4b9a5556d2f9cd6c4dd615bdb5ea28
妹尾弦 <gengineer.web@gmail.com>
2020-07-08 00:04:31 +0900

    add: icon buttons stories

---
+++ src/stories/atoms/buttons/ButtonAccountOutlinedIcon.stories.tsx
@@ -0,0 +1,5 @@
+import * as React from 'react'
+import { storiesOf } from '@storybook/react'
+import { ButtonAccountOutlinedIcon } from '@/components/atoms'
+
+storiesOf(`atoms/buttons`, module).add(`ButtonAccountOutlinedIcon`, () => <ButtonAccountOutlinedIcon />)
---
+++ src/stories/atoms/buttons/ButtonHomeOutlinedIcon.stories.tsx
@@ -0,0 +1,5 @@
+import * as React from 'react'
+import { storiesOf } from '@storybook/react'
+import { ButtonHomeOutlinedIcon } from '@/components/atoms'
+
+storiesOf(`atoms/buttons`, module).add(`ButtonHomeOutlinedIcon`, () => <ButtonHomeOutlinedIcon />)

33fc92b1253986e63c17ba9c844bb62d69515b39
妹尾弦 <gengineer.web@gmail.com>
2020-07-10 00:21:29 +0900

    feat: add default layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,15 @@
 import React from 'react'
-import App from '@/components/App'
+import Layout from '@/layout/default'
 import { StButton } from '@/components/StButton'
 import { ButtonCircleAddIcon } from '@/components/atoms/buttons/ButtonCircleAddIcon'
 import { ButtonPrimary } from '@/components/atoms/buttons/ButtonPrimary'
-import { BNavigation } from '@/components/organisms/navigation/BNavigation'
 
 export default function Home(): JSX.Element {
   return (
-    <App>
+    <Layout>
       <StButton>Hello World!!!</StButton>
       <ButtonCircleAddIcon />
       <ButtonPrimary>test</ButtonPrimary>
-      <BNavigation />
-    </App>
+    </Layout>
   )
 }

5a12d903d8781fd18afde65d865e2bc22f3a8485
妹尾弦 <gengineer.web@gmail.com>
2020-07-11 21:06:27 +0900

    add: base of pages

---
+++ src/pages/mypage/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Typography } from '@material-ui/core'
+import Layout from '@/layout/default'
+import { myStyled } from '@/types/themes'
+
+const StyleP = myStyled.p`
+  font-size: 20px;
+  color: ${(props) => props.theme.palette.primary.main};
+  margin-top: ${(props) => props.theme.spacing(32)}px;
+`
+
+export default function Clubs(): JSX.Element {
+  return (
+    <Layout>
+      <StyleP>My Page by TypeScript!</StyleP>
+      <Typography variant='subtitle1'>test</Typography>
+    </Layout>
+  )
+}

9888d636f8d6b29d9ff48fa7e7c7a73c4091a9e9
妹尾弦 <gengineer.web@gmail.com>
2020-07-12 18:55:12 +0900

    modify: change configs

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Link from 'next/link'
+import Link from '@/components/utils/Link'
 
 type IHeader = {
   pathname?: string
@@ -7,11 +7,11 @@ type IHeader = {
 
 export const Header: React.FCX<IHeader> = ({ pathname }) => (
   <header>
-    <Link href='/'>
+    <Link href='/' color='secondary'>
       <a className={pathname === `/` ? `is-active` : ``}>Home</a>
     </Link>
     {` `}
-    <Link href='/about'>
+    <Link href='/about' color='secondary'>
       <a className={pathname === `/about` ? `is-active` : ``}>About</a>
     </Link>
   </header>

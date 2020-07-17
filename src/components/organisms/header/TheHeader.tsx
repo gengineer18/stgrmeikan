5ceae55ad248374d8a7951414f13c7506a06d1b4
妹尾弦 <gengineer.web@gmail.com>
2020-07-17 21:18:09 +0900

    feat: add header

---
+++ src/components/organisms/header/TheHeader.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import { ButtonMenuIcon, ButtonLoginIcon } from '@/components/atoms'
+
+// styles
+import { colorPrimaryMain } from '@/styles/variable'
+import { baseInnerWidth } from '@/styles/common'
+import { css } from '@emotion/core'
+
+const styleAppBar = css({
+  position: `fixed`,
+  left: `0px`,
+  top: `0px`,
+  width: `100%`,
+  padding: `8px 0`,
+  zIndex: 100,
+  backgroundColor: `${colorPrimaryMain}`,
+})
+
+const layout = css({
+  flexGrow: 1,
+})
+
+export const TheHeader: React.FCX = () => (
+  <AppBar position='fixed' css={styleAppBar}>
+    <Toolbar css={baseInnerWidth}>
+      <div css={layout}>
+        <ButtonMenuIcon />
+      </div>
+      <figure css={layout}>
+        <img alt='J-StaGourmet' />
+      </figure>
+      <ButtonLoginIcon />
+    </Toolbar>
+  </AppBar>
+)
--- src/components/organisms/index.ts
+++ src/components/organisms/index.ts
@@ -1,2 +1,5 @@
+// header
+export { TheHeader } from '@/components/organisms/header/TheHeader'
+
 // navigation
 export { BNavigation } from '@/components/organisms/navigation/BNavigation'

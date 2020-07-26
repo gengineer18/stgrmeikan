2e6eabf7f42f573f0baeba2de5bffa5fb2535f54
妹尾弦 <gengineer.web@gmail.com>
2020-07-26 23:05:32 +0900

    リスト編集・画像追加

--- src/pages/clubs/index.tsx
+++ src/pages/clubs/index.tsx
@@ -1,40 +1,16 @@
 import React from 'react'
 import App from '@/components/App'
-import { Avatar, Collapse, List } from 'antd'
-import Link from '@/components/atom/Link'
 import clubs from '@/assets/json/AllClubs.json'
 import { ClubData } from '@/types/json'
 
-const { Panel } = Collapse
+import { ClubList } from '@/components/presentational'
 
 export default function Clubs({ data }: { data: ClubData }): JSX.Element {
   return (
     <App>
-      <Collapse ghost>
-        {data.clubData.map((element) => (
-          <Panel header={element.areaJp} key={element.id}>
-            <List
-              itemLayout='horizontal'
-              dataSource={element.list}
-              renderItem={(item) => (
-                <Link href='/clubs/[clubid]' as={`/clubs/${item.clubId}`}>
-                  <List.Item>
-                    <List.Item.Meta
-                      avatar={
-                        // eslint-disable-next-line react/jsx-wrap-multilines
-                        <Avatar shape='square' src={`/static/images/clubs/${item.clubId}.png`}>
-                          {item.clubId}
-                        </Avatar>
-                      }
-                      title={item.name}
-                    />
-                  </List.Item>
-                </Link>
-              )}
-            />
-          </Panel>
-        ))}
-      </Collapse>
+      {data.clubData.map((area) => (
+        <ClubList data={area} key={area.id} />
+      ))}
     </App>
   )
 }

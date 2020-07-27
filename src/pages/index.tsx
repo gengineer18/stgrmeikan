2ebe2d6f86b89836cf1e550f3187875f2165eea5
妹尾弦 <gengineer.web@gmail.com>
2020-07-27 19:24:18 +0900

    不要ファイルの削除

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,11 @@
 import React from 'react'
 import App from '@/components/App'
-import { StButton } from '@/components/StButton'
 import { ButtonPrimary } from '@/components/atom/buttons/ButtonPrimary'
 import Link from '@/components/atom/Link'
 
 export default function Home(): JSX.Element {
   return (
     <App>
-      <StButton>Hello World!!!</StButton>
       <Link href='/post/[postId]' as='/post/test'>
         <ButtonPrimary>test</ButtonPrimary>
       </Link>

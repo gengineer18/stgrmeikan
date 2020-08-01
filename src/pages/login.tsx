ca01ffbfaa6d76abfb2da28fe2cee34cc7367983
妹尾弦 <gengineer.web@gmail.com>
2020-08-02 00:03:34 +0900

    feat: layouted login page

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,23 +1,58 @@
 import React from 'react'
 import App from '@/components/App'
+import { Typography, Space, Divider } from 'antd'
 import { LoginButtonTwitter, LoginButtonFacebook, LoginButtonGoogle } from '@/components/presentational'
 import { css } from '@emotion/core'
+import NextLink from 'next/link'
 
-const style = css({
-  marginTop: `8px`,
+const { Title, Paragraph, Link } = Typography
+
+const styledWrap = css({
+  maxWidth: `560px`,
+  margin: `16px auto`,
+  textAlign: `center`,
+})
+
+const styledTitle = css({
+  fontSize: `1.8rem`,
+})
+
+const styledSpace = css({
+  display: `block`,
 })
 
 export default function Login(): JSX.Element {
   return (
     <App>
-      <div css={style}>
-        <LoginButtonTwitter />
-      </div>
-      <div css={style}>
-        <LoginButtonFacebook />
-      </div>
-      <div css={style}>
-        <LoginButtonGoogle />
+      <div css={styledWrap}>
+        <Title>
+          <span css={styledTitle}>ログイン</span>
+        </Title>
+        <Divider />
+        <Space direction='vertical' css={styledSpace} size='middle'>
+          <LoginButtonTwitter />
+          <LoginButtonFacebook />
+          <LoginButtonGoogle />
+        </Space>
+        <Divider />
+        <Paragraph>
+          ログインすることにより、
+          <br />
+          <Link href='/terms' target='_blank'>
+            利用規約
+          </Link>
+          および
+          <Link href='/privacy' target='_blank'>
+            プライバシーポリシー
+          </Link>
+          に同意したとみなされます。
+        </Paragraph>
+        <Paragraph>
+          <NextLink href='/signup'>
+            <a>新規会員登録</a>
+          </NextLink>
+          はこちら
+        </Paragraph>
       </div>
     </App>
   )

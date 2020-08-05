aa2771c4e4f0991d8a5c5aae0fc718bfe4c420af
妹尾弦 <gengineer.web@gmail.com>
2020-08-05 10:04:14 +0900

    型定義

--- src/client/form/homeTeamTreeData.ts
+++ src/client/form/homeTeamTreeData.ts
@@ -1,12 +1,26 @@
 import clubs from '@/assets/json/AllClubs.json'
 
-export const treeData = clubs.clubData.map((area) => {
-  const title = area.areaJp
-  const value = area.areaEn
-  const children = area.list.map((club) => {
-    const title = club.name
-    const value = club.clubId
+import { Area, Club } from '@/types/json'
+
+interface ITreeData {
+  title: string
+  value: string
+  children: {
+    title: string
+    value: string
+  }[]
+}
+
+const createChildren = (area: Area) =>
+  area.list.map((club: Club) => {
+    const title: string = club.name
+    const value: string = club.clubId
     return { title, value }
   })
+
+export const treeData: ITreeData[] = clubs.clubData.map((area: Area) => {
+  const title: string = area.areaJp
+  const value: string = area.areaEn
+  const children: { title: string; value: string }[] = createChildren(area)
   return { title, value, children }
 })

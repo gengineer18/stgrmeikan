d160b0c5804d67c9e5583d711ae1dfc66f00de81
gengineer18 <51533184+gengineer18@users.noreply.github.com>
2020-08-08 20:27:00 +0900

    Feat/club page (#33)
    * fix: club page layout
    
    * fix: 軽微な修正

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,8 +5,8 @@ import { Grid } from '@material-ui/core'
 import { CardOverview } from '@/components/commonPresentational/card/CardOverview'
 
 export default function Home(): JSX.Element {
-  const tag = (
-    <Grid item xs={12} sm={6} key='1'>
+  const tag = (key) => (
+    <Grid item xs={12} sm={6} key={key}>
       <Link href='/post/[postId]' as='/post/test'>
         <a>
           <CardOverview />
@@ -14,11 +14,7 @@ export default function Home(): JSX.Element {
       </Link>
     </Grid>
   )
-  const array = []
-  // eslint-disable-next-line no-plusplus
-  for (let i = 0; i < 5; i++) {
-    array.push(tag)
-  }
+  const array = [...Array(6)].map((_, i) => tag(i))
   return (
     <App>
       <Grid container spacing={3}>

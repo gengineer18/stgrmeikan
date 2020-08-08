ab4931e5332bf42c3ef6ee65b31323bd85bddeda
妹尾弦 <gengineer.web@gmail.com>
2020-08-08 20:20:49 +0900

    fix: club page layout

--- src/pages/clubs/[clubid].tsx
+++ src/pages/clubs/[clubid].tsx
@@ -1,19 +1,25 @@
 import React from 'react'
-import { Typography } from '@material-ui/core'
 import App from '@/components/App'
-import { myStyled } from '@/types/themes'
-
-const StyleP = myStyled.p`
-  font-size: 20px;
-  color: ${(props) => props.theme.palette.primary.main};
-  margin-top: ${(props) => props.theme.spacing(32)}px;
-`
+import Link from 'next/link'
+import { Grid } from '@material-ui/core'
+import { CardOverview } from '@/components/commonPresentational/card/CardOverview'
 
 export default function ClubId(): JSX.Element {
+  const tag = (key) => (
+    <Grid item xs={12} sm={6} key={key}>
+      <Link href='/post/[postId]' as='/post/test'>
+        <a>
+          <CardOverview />
+        </a>
+      </Link>
+    </Grid>
+  )
+  const array = [...Array(6)].map((_, i) => tag(i))
   return (
     <App>
-      <StyleP>ClubId Page by TypeScript!</StyleP>
-      <Typography variant='subtitle1'>test</Typography>
+      <Grid container spacing={3}>
+        {array.map((tag) => tag)}
+      </Grid>
     </App>
   )
 }
